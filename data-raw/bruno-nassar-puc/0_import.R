f0b3a67b09d3096ceca99611fe85377d9d2b981d
rcfeliz <ric.feliz@gmail.com>
2022-10-24 17:15:14 -0300

    att da base e dos plots ja feitos (ate o 23b)

--- data-raw/bruno-nassar-puc/0_import.R
+++ data-raw/bruno-nassar-puc/0_import.R
@@ -1,5 +1,5 @@
 # import
 
-da <- readr::read_csv("/home/abj/Downloads/RESPOSTAS AO FORMS (parte 1) - Processos.csv")
-
+# da <- readr::read_csv("/home/abj/Downloads/RESPOSTAS AO FORMS (parte 1) - Processos.csv")
+da <- readr::read_csv("/home/abj/Downloads/respostas_2.csv")
 readr::write_rds(da, "data-raw/bruno-nassar-puc/data-raw/homicidios_bruto.rds")
