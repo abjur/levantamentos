efb71a232413a5df8969512f2e84fa45ad2160b2
rcfeliz <ric.feliz@gmail.com>
2022-11-29 20:37:44 -0300

    base juliana bumachar

--- data-raw/nepi_2022/bases_bumachar.R
+++ data-raw/nepi_2022/bases_bumachar.R
@@ -1,20 +1,20 @@
 # Bases Juliana Bumachar
 
 da <- obsFase3::da_processo_tidy |>
+  dplyr::mutate(info_autofal = stringr::str_to_sentence(info_autofal)) |>
   dplyr::filter(
-    (info_fal_dec == "Sim" | info_conv2 != "Não") &
-    (info_fal_acabou2 == "Sim" | info_fal_acabou == "Sim")
+    info_fal_dec_fund == "Artigo 97-I ou 105 da Lei 11.101/2005 (autofalencia)" |
+    info_origem == "Falência requerida pela própria empresa (autofalência)"
   ) |>
   dplyr::mutate(
     info_conv2 = dplyr::case_when(
       info_conv2 == "artigo 99, da Lei nº 11.101/2005" ~ "Sim, com fundmento no artigo 99, da Lei nº 11.101/2005",
       TRUE ~ info_conv2
-    ),
-    info_fal_acabou = "Sim"
+    )
   ) |>
   dplyr::select(
     id_processo,
-    info_conv2,
+    info_conv = info_conv2,
     info_autofal,
     dt_decisao,
     info_fal_dec,
