872b4c4298a5d52fcd905bc8bebe0225ac895e1c
rmhirota <re.hirota@gmail.com>
2023-02-08 12:00:03 -0300

    bases maria nepi 2022

--- data-raw/nepi_2022/bases_maria.R
+++ data-raw/nepi_2022/bases_maria.R
@@ -1,12 +1,25 @@
 da <- obsFase3::da_processo_tidy |>
   dplyr::filter(
-    ano_dist >= 2015 & ano_dist <= 2020,
-    info_fal_dec_nao == "Acordo extrajudicial"
+    # ano_dist >= 2015 & ano_dist <= 2020,
+    info_fal_dec_nao %in% c(
+      "Acordo extrajudicial",
+      "Abandono/Desistência/Falta de interesse da requerente"
+    )
+  ) |>
+  dplyr::mutate(
+    ano_dist = ifelse(ano_dist == "2015, 2015", "2015", ano_dist)
   ) |>
   dplyr::select(
     id_processo,
-    dt_extincao,
-    info_fal_dec_nao
+    ano_dist,
+    info_classe,
+    info_assunto,
+    info_foro,
+    info_fal_dec_fund,
+    dt_fal_fim,
+    dt_dist,
+    info_origem,
+    info_obrig_extin
   )
 
 writexl::write_xlsx(da, "data-raw/nepi_2022/xlsx/da_maria.xlsx")
