72f64ac879a1e6cec5e4ac9e01e18a72ec27feae
rcfeliz <ric.feliz@gmail.com>
2024-10-22 14:28:37 +0000

    dados leiloes e upis bernardo e davi

--- data-raw/davi-bernardo-paris/extracao_cnpjs.R
+++ data-raw/davi-bernardo-paris/extracao_cnpjs.R
@@ -1,24 +1,54 @@
-cnpjRJRJ <- obsRJRJ::aux_rfb |>
-  dplyr::distinct(cnpj) |>
-  dplyr::transmute(
-    tj = "RJ",
-    cnpj
-  )
+daRJRJ <- obsRJRJ::da_parte_tidy |>
+  dplyr::distinct(id_processo, cnpj) |>
+  dplyr::left_join(
+    x = obsRJRJ::da_rjrj |>
+      dplyr::select(id_processo, dplyr::contains("leilao"), upi_teve) |>
+      dplyr::mutate(
+        upi_vendeu = NA_character_
+      ),
+    by = "id_processo"
+  ) |>
+  dplyr::mutate(
+    tj = "RJ"
+  ) |>
+  dplyr::relocate(cnpj, .after = "id_processo") |>
+  dplyr::relocate(tj, .after = "cnpj")
 
-cnpjRJSP <- obsFase2::da_relatorio |>
-  dplyr::distinct(cnpj) |>
+daRJSP <- obsFase2::da_relatorio |>
   dplyr::transmute(
-    tj = "SP",
-    cnpj
-  )
+    id_processo = n_processo,
+    cnpj,
+    leilao_teve = leilao,
+    leilao_data = as.Date(NA),
+    leilao_vendeu = NA_character_,
+    upi_teve = upi,
+    upi_vendeu
+  ) |>
+  tidyr::separate_rows(cnpj, sep = ", ") |>
+  dplyr::mutate(
+    cnpj = cnpj |>
+      abjutils::clean_cnj() |>
+      stringr::str_squish(),
+    tj = "SP"
+  ) |>
+  dplyr::relocate(tj, .after = "cnpj")
 
-cnpjRJRS <- obsRJRS::aux_rfb |>
-  dplyr::distinct(cnpj) |>
-  dplyr::transmute(
-    tj = "RS",
-    cnpj
-  )
+daRJRS <- obsRJRS::da_parte_tidy |>
+  dplyr::distinct(id_processo, cnpj) |>
+  dplyr::left_join(
+    x = obsRJRS::da_rjrs |>
+      dplyr::select(id_processo, leilao_teve, leilao_data, leilao_vendeu, upi_teve) |>
+      dplyr::mutate(
+        upi_vendeu = NA_character_
+      ),
+    by = "id_processo"
+  ) |>
+  dplyr::mutate(
+    tj = "RS"
+  ) |>
+  dplyr::relocate(cnpj, .after = "id_processo") |>
+  dplyr::relocate(tj, .after = "cnpj")
 
-cnpjs <- dplyr::bind_rows(cnpjRJRJ, cnpjRJSP, cnpjRJRS)
+cnpjs <- dplyr::bind_rows(daRJRJ, daRJSP, daRJRS)
 
 readr::write_csv(cnpjs, "data-raw/davi-bernardo-paris/cnpjs.csv")
